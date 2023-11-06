acbfa808d99241b326b8590530f0a225c50a26d6
Medha Narayan <medhanarayanccs@gmail.com>
2023-11-07 02:32:46 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import AgeCalculator from './components/AgeCalculator';
+import './App.css';
+
+const App = () => {
+  return (
+    <div>
+      <AgeCalculator />
+    </div>
+  );
+};
+
+export default App;
